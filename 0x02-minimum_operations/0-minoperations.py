0b5dd415ea96ace120236f29e9810f43d249c822
3SSI3 <starsm004@gmail.com>
2023-07-13 22:44:28 +0300

    Minoperatios

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Calculates the fewest number of operations needed
+to result in exactly n H characters in the file.
+"""
+
+def minOperations(n):
+    now = 1
+    start = 0
+    counter = 0
+    while now < n:
+        remainder = n - now
+        if (remainder % now == 0):
+            start = now
+            now += start
+            counter += 2
+        else:
+            now += start
+            counter += 1
+    return counter
