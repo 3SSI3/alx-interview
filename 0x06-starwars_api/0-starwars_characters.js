e79797a53f0fd665474e9b4a3c87324756176c2b
3SSI3 <starsm004@gmail.com>
2023-08-11 03:36:31 +0300

    Star Wars Characters:rocket:

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,36 @@
 #!/usr/bin/node
+
 const request = require('request');
+
 const movieId = process.argv[2];
+
 const options = {
-  url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
+  url: `https://swapi.dev/api/films/${movieId}/`,
   method: 'GET'
 };
 
 request(options, function (error, response, body) {
   if (!error) {
-    const characters = JSON.parse(body).characters;
+    const filmData = JSON.parse(body);
+    const characters = filmData.characters;
     printCharacters(characters, 0);
+  } else {
+    console.error('Error:', error);
   }
 });
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
+function printCharacters(characters, index) {
+  if (index >= characters.length) {
+    return;
+  }
+
+  request(characters[index], function (charError, charResponse, charBody) {
+    if (!charError) {
+      const characterData = JSON.parse(charBody);
+      console.log(characterData.name);
+      printCharacters(characters, index + 1);
+    } else {
+      console.error('Error fetching character:', charError);
     }
   });
 }
