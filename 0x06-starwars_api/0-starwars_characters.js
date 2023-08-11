24e8046b27acdb428274b408edb78f85a24d77b7
3SSI3 <starsm004@gmail.com>
2023-08-11 03:25:28 +0300

    Star Wars Characters:rocket:

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,44 +1,25 @@
 #!/usr/bin/node
-
 const request = require('request');
-
 const movieId = process.argv[2];
+const options = {
+  url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
+  method: 'GET'
+};
 
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
+request(options, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
-
-  if (response.statusCode !== 200) {
-    console.error('Failed to retrieve movie information:', response.statusCode);
-    return;
-  }
-
-  const characters = JSON.parse(body).characters;
-  printCharacters(characters, 0);
 });
 
-function printCharacters(characters, index) {
-  if (index >= characters.length) {
-    return;
-  }
-
-  request(characters[index], (charError, charResponse, charBody) => {
-    if (charError) {
-      console.error('Error fetching character:', charError);
-      return;
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
     }
-
-    if (charResponse.statusCode !== 200) {
-      console.error('Failed to retrieve character information:', charResponse.statusCode);
-      return;
-    }
-
-    const characterName = JSON.parse(charBody).name;
-    console.log(characterName);
-    printCharacters(characters, index + 1);
   });
 }
