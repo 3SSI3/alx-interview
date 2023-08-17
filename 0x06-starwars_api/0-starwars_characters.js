c8304a1d4820b1f0fca51d142c83b26b7bd64fb2
langatfestus <langatfestus94@gmail.com>
2023-08-18 00:58:49 +0300

    additions

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,35 +2,16 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
-
-const options = {
-  url: `https://swapi.dev/api/films/${movieId}/`,
-  method: 'GET'
-};
-
-request(options, function (error, response, body) {
-  if (!error) {
-    const filmData = JSON.parse(body);
-    const characters = filmData.characters;
-    printCharacters(characters, 0);
-  } else {
-    console.error('Error:', error);
-  }
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
-
-function printCharacters (characters, index) {
-  if (index >= characters.length) {
-    return;
-  }
-
-  request(characters[index], function (charError, charResponse, charBody) {
-    if (!charError) {
-      const characterData = JSON.parse(charBody);
-      console.log(characterData.name);
-      printCharacters(characters, index + 1);
-    } else {
-      console.error('Error fetching character:', charError);
-    }
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
   });
-}
+};
