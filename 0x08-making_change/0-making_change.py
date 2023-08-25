5d6fd78049cad73c995a7f6a4e611b6b76f0b388
3SSI3 <starsm004@gmail.com>
2023-08-25 04:48:07 +0300

    Making Change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-def makeChange(coins, total):
-    if total <= 0:
-        return 0
-    
-    # Initialize an array to store minimum coin counts
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-    
-    # Iterate through each coin denomination
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-    
-    return dp[total] if dp[total] != float('inf') else -1
-=======
 #!/usr/bin/python3
 
 """ Contains makeChange function"""
